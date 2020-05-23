4802d30f1bd8aebe7442cc98aeb2307674d1188f
damon-kwok <damon-kwok@outlook.com>
2020-05-23 17:11:54 +0800

    test

--- tail.go
+++ tail.go
@@ -15,9 +15,9 @@ import (
 	"sync"
 	"time"
 
-	"github.com/hpcloud/tail/ratelimiter"
-	"github.com/hpcloud/tail/util"
-	"github.com/hpcloud/tail/watch"
+	"github.com/damon-kwok/tail/ratelimiter"
+	"github.com/damon-kwok/tail/util"
+	"github.com/damon-kwok/tail/watch"
 	"gopkg.in/tomb.v1"
 )
 
